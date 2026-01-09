35e450dd9d4604c0bf6ba9c40dbddb07dc1ee720
aman-Tomar-30 <aman.23bcon0600@jecrcu.edu.in>
2026-01-09 23:16:17 +0530

    Add schema file

---
+++ models.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+connection = sqlite3.connect("example.db")
+cursor = connection.cursor()
+cursor.execute("PRAGMA foreign_keys = ON;")
+
+# Create users table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+""")
+
+# Create diary entries table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS entries (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+""")
+
+connection.commit()
+connection.close()
